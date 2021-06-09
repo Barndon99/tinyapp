c94262b1f40706be0edb3ab3dc34a78931d87051
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-08 21:13:17 -0600

    Added login/logout paths, cleared the database

--- expressServer.js
+++ expressServer.js
@@ -10,8 +10,7 @@ app.use(cookieParser());
 
 //Declare Database variable
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+
 };
 
 //Generate Short URL value
