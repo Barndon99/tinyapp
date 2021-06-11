a6b804d2c85f36b73c825ee663e71b490234de52
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-10 21:36:04 -0600

    Final update, all links are working, functionality is good

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,5 @@
 const express = require("express");
-var cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
 const checkIfEmailIsAlreadyUsed = require("./helper");
 
@@ -9,10 +9,12 @@ const PORT = 8080; // default port 8080
 
 // enable ejs
 app.set('view engine', 'ejs');
+//Set up encrypted cookies
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
 }));
+//Include url encoding
 app.use(express.urlencoded({extended: false}));
 
 //Declare Database variable
@@ -23,34 +25,21 @@ const urlDatabase = {
 
 
 //Include a users object
-let users = { 
+let users = {
   "Greg": {
-    user_id: "Greg", 
-    email: "g@g.com", 
+    user_id: "Greg",
+    email: "g@g.com",
     password: "12345"
   },
- "Harold": {
-    user_id: "Greg", 
-    email: "h@h.com", 
+  "Harold": {
+    user_id: "Greg",
+    email: "h@h.com",
     password: "12345"
   }
 };
 
-//const checkIfEmailIsAlreadyUsed = function (email, database) {
-//  const keys = Object.keys(users);
-//  console.log("Keys: ", keys)
-//  for (const key of keys) {
-//    if (database[key].email === email) {
-//      console.log("These are the users: ", users);
-//      return database[key].user_id;
-//    }
-//  }
-//  
-//  return null;
-//};
-//
-//Generate Short URL value
-function generateRandomString() {
+//Helper function to generate a string
+generateRandomString = () => {
   const string = Math.random().toString(36).slice(2);
   return string.slice(2, 8);
 };
@@ -59,17 +48,19 @@ function generateRandomString() {
 
 //Redirects to longURL
 app.get("/u/:shortURL", (req, res) => {
-  console.log("params: ", req.params.shortURL);
   const redirectURL = urlDatabase[req.params.shortURL].longURL;
-  
   res.redirect(302, redirectURL);
 });
 
 //Collect URLS on our home page and connect them to views
 app.get('/urls', (req, res) => {
+  //redirect to login if not logged in
+  if (!req.session.user_id) {
+    return res.redirect(302, "/urls/login");
+  }
   //Pass in ID as a parameter
-  const filteredData = function (urlDatabase) {
-    const filteredURLS = {}
+  const filteredData = function(urlDatabase) {
+    const filteredURLS = {};
     for (const url in urlDatabase) {
       if (urlDatabase[url].userID === req.session.user_id) {
         filteredURLS[url] = urlDatabase[url];
@@ -77,75 +68,71 @@ app.get('/urls', (req, res) => {
     }
     return filteredURLS;
   };
-  //let newURLS = filteredData(urlDatabase);
-  
-  //console.log("newURLS: ", newURLS)
-  
+
   const templateVars = { urls: filteredData(urlDatabase), user: users[req.session.user_id]};
 
   if (!templateVars.user) {
     templateVars.urls = {};
-  };
-  //console.log("urldata: ", newURLS);
+  }
+
   res.render('urls_index', templateVars);
 });
 
 //Create a new form for submitting URLS to be shortened
 app.get("/urls/new", (req, res) => {
-  if(!req.session.user_id) {
-     return res.redirect(302, "/urls/login");
+  if (!req.session.user_id) {
+    return res.redirect(302, "/urls/login");
   }
-  console.log(req.session.user_id);
-  const templateVars = {user: users[req.session.user_id]}
+
+  const templateVars = {user: users[req.session.user_id]};
   res.render("urls_new", templateVars);
 });
 
 //Add a registration Page
 app.get("/urls/register", (req, res) => {
-  const templateVars = {user: users[req.session.user_id]}
+  const templateVars = {user: users[req.session.user_id]};
   res.render("url_register", templateVars);
 });
 
-//Add a login Page
+//Renders the login Page
 app.get("/urls/login", (req, res) => {
-  const templateVars = {user: users[req.session.user_id]}
-
+  const templateVars = {user: users[req.session.user_id]};
   res.render("urls_login", templateVars);
 });
 
 //Generate individul pages for shortURLS connecting to urls_show.ejs
 app.get("/urls/:shortURL", (req, res) => {
-  //console.log(urlDatabase[req.params.shortURL].longURL);
+  if (urlDatabase[req.params.shortURL] === undefined) {
+    return res.status(404).send("This shortURL doesn't exist! Sorry, try again?");
+  }
+
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.user_id] };
-  //console.log(templateVars);
+
   res.render("urls_show", templateVars);
 });
 
 //Path for edit buttons
 app.get("/urls/:shortURL/goto", (req, res) => {
   shortURL = req.params.shortURL;
-  //console.log("urlID: ", urlDatabase[shortURL], "userID: ", req.cookies["user_id"]);
   if (urlDatabase[shortURL].userID !== req.session.user_id) {
     return res.write("Log in to edit Links").redirect(302, "/urls");
   }
-  res.redirect(302, `/urls/${shortURL}`);         
+  res.redirect(302, `/urls/${shortURL}`);
 });
 
 // Edits long URL
 app.post("/urls/:shortURL", (req, res) => {
   //Creat if logic to abort if unefined, return 404 or some error
   urlDatabase[req.params.shortURL].longURL = req.body.newLongURL;
-  res.redirect(302, '/urls')
+  res.redirect(302, '/urls');
 });
 
 //Post new URLS to our database
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  //console.log("cookie: ", res.cookies, "longURL", req.body.longURL);
   //This bit is pretty sus we can try again in the morning. User id won't be defined no matter what I do... idk...
   urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.session.user_id};
-  console.log(urlDatabase);
-  res.redirect(`/urls/${shortURL}`);         
+  res.redirect(`/urls/${shortURL}`);
 });
 
 //Adds new user when they register
@@ -153,26 +140,22 @@ app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const id = generateRandomString();
-  console.log("email: ", email, "password: ", password, "id: ", id);
   //Hash the password
   const salt = bcrypt.genSaltSync(5);
   const hash = bcrypt.hashSync(password, salt);
-  console.log("Register hash: ", hash);
   //Ensures an email and password were provided (completely arbitrary since both are already required in ejs file)
   if (email === "" || password === "") {
     return res.send("Error must provide an email and password");
   }
   //Ensure that the user does not already exist
- 
   if (checkIfEmailIsAlreadyUsed(email, users)) {
     return res.send(400, "Email already in use");
   }
 
   users[id] = { email, password: hash, user_id: id };
-  console.log("User ID at registration: ", users[id], "All Users: ", users);
-  //res.session("user_id", id);
+
   req.session.user_id = id;
-  res.redirect(302, '/urls')
+  res.redirect(302, '/urls');
 });
 
 //Saves a cookie for new users
@@ -183,12 +166,11 @@ app.post("/login", (req, res) => {
   if (id === null) {
     return res.send(403, "Email doesn't exist");
   }
-  console.log("users login:", users, "CheckEmail ID: ", id);
+
   const passHashed = users[id].password;
 
   if (checkIfEmailIsAlreadyUsed(email, users) !== null) {
     if (!bcrypt.compareSync(password, passHashed)) {
-      console.log("Inside checkPass");
       return res.send(403, "Passwords do not match");
     }
   } else {
@@ -201,10 +183,9 @@ app.post("/login", (req, res) => {
 
 //Adds logout functionality
 app.post("/logout", (req, res) => {
-  //res.clearCookie("user_id");
   req.session = null;
   res.redirect(302, "/urls");
-})
+});
 
 //Delete a tinyURL *This part works, but the button is broken !! Fixed the button, but have to refresh the page to see changes
 app.post('/urls/:shortURL/delete', (req, res) => {
